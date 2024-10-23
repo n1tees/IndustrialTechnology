b6974fd987446aa441f53dc6decc6761a913e9b2
Nick S <116270750+n1tees@users.noreply.github.com>
2024-10-23 16:15:47 +0300

    Create task_1.go
    1. Перевод чисел из одной системы счисления в другую  
       Напишите программу, которая принимает на вход число в произвольной системе счисления (от 2 до 36) и переводит его в другую систему счисления.  
       *Input:* число, исходная система, конечная система.  
       *Output:* число в конечной системе.

---
+++ task_1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "bufio"
+    "errors"
+    "fmt"
+    "math/big"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+
+    fmt.Println("enter num, currnet system num and target system")
+    scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    input := scanner.Text()
+
+    str := strings.Split(input, " ")
+    value := str[0]
+
+    var number [2]int
+    for i := 1; i < len(str); i++ {
+        var err error
+        number[i-1], err = strconv.Atoi(str[i])
+
+        if err != nil {
+            fmt.Println("Error during conversion, try to enter values again")
+            return
+        }
+    }
+
+    var err error
+    output, err := convertBase(value, number[0], number[1])
+
+    if err == nil {
+        fmt.Print("your number in target system is " + output)
+        return
+    } else { fmt.Println(err) }
+}
