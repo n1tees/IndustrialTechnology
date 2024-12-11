399696bbf945d87d64fd63ae608333295cde2838
Nick S <116270750+n1tees@users.noreply.github.com>
2024-12-11 14:19:43 +0300

    Update task_4_4.go

--- task_4_4.go
+++ task_4_4.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var n int
+	fmt.Print("Введите количество строк: ")
+	fmt.Scan(&n)
+	arr := make([]string, n)
+	fmt.Println("Введите строки:")
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	var result string
+	for i := 0; i < n; i++ {
+		result += arr[i]
+		if i < n-1 {
+			result += " "
+		}
+	}
+	fmt.Println("Объединенная строка:", result)
+}
