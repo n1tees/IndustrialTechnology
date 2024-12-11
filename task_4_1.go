714b3d510516d09e1508d50c07195f6ae2646adc
Nick S <116270750+n1tees@users.noreply.github.com>
2024-12-11 14:17:43 +0300

    Update and rename task_1.go to task_4_1.go

---
+++ task_4_1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+func main() {
+	var number int
+	fmt.Print("Введите целое число: ")
+	fmt.Scan(&number)
+	if number < 0 {
+		number = -number
+	}
+	sum := sumDigits(number)
+	fmt.Println("Сумма цифр числа:", sum)
+}
